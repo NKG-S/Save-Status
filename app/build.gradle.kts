83ba1187f2dc0aa347cb453819859ebd29c3fdac
Nethmin <nethminkavindu@gmail.com>
2025-07-28 22:33:37 +0530

    images can resize

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,10 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"),"proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
@@ -46,25 +49,38 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Material Components
     implementation("com.google.android.material:material:1.12.0")
+
+    // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.10")
+
+    // AndroidX
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.fragment:fragment-ktx:1.8.8")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     implementation("androidx.preference:preference-ktx:1.2.1")
+
+    // Image Loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+
+    // Video Player
     implementation("com.google.android.exoplayer:exoplayer:2.19.1")
     implementation("com.google.android.exoplayer:exoplayer-ui:2.19.1")
-}
 
+    // PhotoView for zoomable images
+    implementation("com.github.Baseflow:PhotoView:2.3.0")
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
 
 configurations.all {
     resolutionStrategy {
         force("xmlpull:xmlpull:1.1.3.1")
     }
 }
-
