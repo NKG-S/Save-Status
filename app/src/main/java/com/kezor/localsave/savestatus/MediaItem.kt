87f22defa928cf775ad10801b8b8f4f962d90233
Nethmin <nethminkavindu@gmail.com>
2025-07-29 21:51:00 +0530

    change file - ok save changed file = ok show in saved ui = ok saved ui delete function = ok

--- app/src/main/java/com/kezor/localsave/savestatus/MediaItem.kt
+++ app/src/main/java/com/kezor/localsave/savestatus/MediaItem.kt
@@ -6,8 +6,8 @@ import java.io.File
 
 @Parcelize
 data class MediaItem(
-    val uri: String,
     val file: File?,
+    val uri: String,
     val type: String, // "image" or "video"
     val lastModified: Long,
     var isSelected: Boolean = false // For selection mode in SavedFragment
